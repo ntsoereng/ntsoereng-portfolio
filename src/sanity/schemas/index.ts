2d7554fa8d00dbb071810f761407a7b5c50c4493
ntsoereng <35761040+ntsoereng@users.noreply.github.com>
2023-10-10 20:48:04 +0200

    Remove unnecessary schema

--- src/sanity/schemas/index.ts
+++ src/sanity/schemas/index.ts
@@ -1,6 +1,5 @@
 import experience from './experience-schema';
-import page from './page-schema';
 import profile from './profile-schema';
 import project from './project-schema';
 
-export const schemaTypes = [profile, page, experience, project];
+export const schemaTypes = [profile, experience, project];
